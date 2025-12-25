e688b4831b4cb08859303ea5a415b39c0f6658e2
Parth <50496941+Parth517@users.noreply.github.com>
2025-12-25 17:25:09 +0000

    docker_compose_and_adding_data_to_postgres

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -3,7 +3,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
-
+import click
 
 prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
    
@@ -39,22 +39,30 @@ df = pd.read_csv(
     nrows=100,
     parse_dates=parse_dates
 )
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 
-
-def run():
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
     year=2021
     month=1
 
-    pg_user ='root'
-    pg_password ='root'
-    pg_host ='localhost'
-    pg_port =5432
-    pg_db ='ny_taxi'
-    chunksize=100000
-    target_table='yellow_taxi_data'
-    
+    # pg_user ='root'
+    # pg_password ='root'
+    # pg_host ='localhost'
+    # pg_port =5432
+    # pg_db ='ny_taxi'
+    # chunksize=100000
+    # target_table='yellow_taxi_data'
     url=f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-    engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
     
     df_iter = pd.read_csv(
